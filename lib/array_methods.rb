58b93d17ee07099fddc2c68e2f1fefcf3a27fca2
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-19 07:06:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,11 +15,11 @@ end
 
 
 def find_min_value(array)
-  counter = 0 
-  min_value = []
-  while counter < array.length do 
-    min_value.push(array.min)
-  counter += 1 
-end
-min_value
+  x = 0 
+  array.length.times { |index| 
+  if array[index] < x 
+    x = array[index]
+  end
+  }
+x 
 end
